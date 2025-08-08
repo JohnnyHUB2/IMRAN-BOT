c93be667d047fa6bad4768e18ae0f49f40b9dcdd
JohnnyHUB2 <olufadejohn66@gmail.com>
2025-08-08 21:08:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: '✨IMRAN BOT✨',
   author: '👑 Imran Hacks',
-  botName: '🤖 IMRAN BOT',
-  botOwner: 'Imran Hacks', // Your name
-  ownerNumber: '639691465580', // Set your number here without + symbol.
+  botName: '🤖 KING BOT',
+  botOwner: 'King😈👑', // Your name
+  ownerNumber: '2348033654467', // Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: 'public', // can be 'public' or 'private'
   description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by Imran Hacks.',
